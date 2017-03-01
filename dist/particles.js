456506aebdd9e7d60ab93b44a2ee97190e98efe7
Marc Brüderlin <hello@marcbruederlin.com>
2017-03-01 17:33:10 +0100

    add responsive breakpoints

--- dist/particles.js
+++ dist/particles.js
@@ -3,7 +3,7 @@
  * for particle backgrounds.
  * 
  * @author Marc Bruederlin <hello@marcbruederlin.com>
- * @version 1.0.3
+ * @version 2.0.0
  * @license MIT
  * @see https://github.com/marcbruederlin/particles.js
  */
@@ -12,187 +12,348 @@
 var Particles = (function(window, document) {
   'use strict';
   
-  // Global variables
-  var Plugin, Particle, canvas, context, config;
+  var Plugin, Particle = {};
 
-  Plugin = function() {
-    // Particle storage, all particles will be stored in this array
-    this.storage = [];
+  /**
+   * Description here…
+   * 
+   * @constructor
+   */
+  Plugin = (function() {
+    var instanceId = 0;
 
-    // Default settings, will be overridden by the users options
-    this.defaults = {
-      maxParticles: 100,
-      sizeVariations: 3,
-      speed: 0.5,
-      color: '#000000',
-      minDistance: 120,
-      connectParticles: false
-    };
+    function Plugin() {
+      var _ = this;
 
-    /**
-     * The plugins startup function. This method prepares the global config object,
-     * the canvas and an event handler for resize events.
-     * 
-     * @param {Object} options - the user options
-     */
-    this.init = function(options) {
-      // Setup an event handler for resize events
-      window.addEventListener('resize', this.resize.bind(this), false);
+      _.defaults = {
+        responsive: null,
+        selector: null,
+        maxParticles: 100,
+        sizeVariations: 3,
+        speed: 0.5,
+        color: '#000000',
+        minDistance: 120,
+        connectParticles: false
+      };
 
-      // Check if options and a selector are specified
-      if (options === undefined || !options.selector) {
-        console.warn('particles.js: No selector is specified! Check https://github.com/marcbruederlin/particles.js#options');
-        return false;
-      }
+      _.element = null;
+      _.context = null;
+      _.activeBreakpoint = null;
+      _.breakpoints = [];
+      _.breakpointSettings = [];
+      _.originalSettings = null;
+      _.windowWidth = 0;
+      _.storage = [];
 
-      // Setup the global configuration
-      config = _mergeObjects(this.defaults, options);
-      config.color = _hex2rgb(config.color);
+      _.instanceId = instanceId++;
+    }
 
-      // Setup the canvas
-      canvas = document.querySelector(config.selector);
-      context = canvas.getContext('2d');
+    return Plugin;
+  }());
 
-      // Set the canvas dimensions
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
 
-      // Fill the storage with the given amount of particle instances
-      for (var i = config.maxParticles; i--;) {
-        this.storage.push(new Particle());
-      }
+  /**
+   * Initialize the plugin.
+   * 
+   * @public
+   * @param {Object} settings
+   */
+  Plugin.prototype.init = function(settings) {
+    var _ = this;
 
-      // Run the plugins animation method
-      this.animate();
-    };
+    _.options = _._extend(_.defaults, settings);
+    _.originalSettings = JSON.parse(JSON.stringify(_.options));
 
-    /**
-     * Calls the draw method 60 times per second.
-     */
-    this.animate = function() {
-      this.draw();
-      window.requestAnimFrame(this.animate.bind(this));
-    };
+    _._initializeCanvas();
+    _._initializeEvents();
+    _._initializeStorage();
+    _._registerBreakpoints();
+    _._checkResponsive(true);
 
-    /**
-     * Clears the canvas and calls the draw method of every particle.
-     */
-    this.draw = function() {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      
-      for (var i = this.storage.length; i--;) {
-        var particle = this.storage[i];
-        particle.draw();
-      }
-    
-      this.update();
-    };
+    _._animate();
+  };
 
-    /**
-     * Calculates the position and movement for each particle.
-     */
-    this.update = function() {
-      for (var i = this.storage.length; i--;) {
-        var particle = this.storage[i];
-        
-        particle.x += particle.vx;
-        particle.y += particle.vy;
-        
-        if (particle.x + particle.radius > canvas.width) {
-          particle.x = particle.radius;
-        } else if (particle.x - particle.radius < 0) {
-          particle.x = canvas.width - particle.radius;
+
+  /**
+   * Description here…
+   * 
+   * @private
+   */
+  Plugin.prototype._registerBreakpoints = function() {
+    var _ = this, breakpoint, currentBreakpoint, l, 
+        responsiveSettings = _.options.responsive || null;
+
+    if(typeof responsiveSettings === 'object' && responsiveSettings !== null && responsiveSettings.length) {
+      for(breakpoint in responsiveSettings) {
+        l = _.breakpoints.length - 1;
+        currentBreakpoint = responsiveSettings[breakpoint].breakpoint;
+
+        if(responsiveSettings.hasOwnProperty(breakpoint)) {
+          while(l >= 0) {
+            if(_.breakpoints[l] && _.breakpoints[l] === currentBreakpoint) {
+              _.breakpoints.splice(l, 1);
+            }
+
+            l--;
+          }
+
+          _.breakpoints.push(currentBreakpoint);
+          _.breakpointSettings[currentBreakpoint] = responsiveSettings[breakpoint].settings;
         }
-          
-        if (particle.y + particle.radius > canvas.height) {
-          particle.y = particle.radius;
-        } else if (particle.y - particle.radius < 0) {
-          particle.y = canvas.height - particle.radius;
-        }
-        
-        if (config.connectParticles) {
-          for (var j = i + 1; j < this.storage.length; j++) {
-            var particle2 = this.storage[j];
-          
-            this.distance(particle, particle2);
+      }
+
+      _.breakpoints.sort(function(a, b) {
+        return b-a;
+      });
+    }
+  };
+
+
+  /**
+   * Description here…
+   * 
+   * @private
+   */
+  Plugin.prototype._initializeEvents = function() {
+    var _ = this;
+
+    window.addEventListener('resize', _._resize.bind(_), false);
+  };
+
+
+  /**
+   * Description here…
+   * 
+   * @private
+   */
+  Plugin.prototype._initializeCanvas = function() {
+    var _ = this;
+
+    if(!_.options.selector) {
+      console.warn('particles.js: No selector specified! Check https://github.com/marcbruederlin/particles.js#options');
+      return false;
+    }
+
+    _.element = document.querySelector(_.options.selector);
+    _.context = _.element.getContext('2d');
+
+    _.element.width = window.innerWidth;
+    _.element.height = window.innerHeight;
+  };
+
+
+  /**
+   * Description here…
+   * 
+   * @private
+   */
+  Plugin.prototype._initializeStorage = function() {
+    var _ = this;
+
+    _.storage = [];
+
+    for (var i = _.options.maxParticles; i--;) {
+      _.storage.push(new Particle(_.element, _.context, _.options));
+    }
+  };
+
+
+  Plugin.prototype._checkResponsive = function(forceUpdate) {
+    var _ = this,
+        breakpoint, targetBreakpoint = false,
+        windowWidth = window.innerWidth;
+
+    if(_.options.responsive && _.options.responsive.length && _.options.responsive !== null) {
+      targetBreakpoint = null;
+
+      for(breakpoint in _.breakpoints) {
+        if(_.breakpoints.hasOwnProperty(breakpoint)) {
+          if(windowWidth <= _.breakpoints[breakpoint]) {
+            targetBreakpoint = _.breakpoints[breakpoint];
           }
         }
       }
-    };
 
-    /**
-     * Calculates the distance between p1 and p2
-     */
-    this.distance = function(p1, p2) {
-      var n, r = p1.x - p2.x,
-          dy = p1.y - p2.y;
+      if(targetBreakpoint !== null) {
+        if(_.activeBreakpoint !== null) {
+          if(targetBreakpoint !== _.activeBreakpoint || forceUpdate) {
+            _.activeBreakpoint = targetBreakpoint;
+      
+            if(_.breakpointSettings[targetBreakpoint].disable) {
+              _._disable();
+            } else {
+              _.options = _._extend(_.options, _.breakpointSettings[targetBreakpoint]);
+              _._refresh();
+            }
+          }
+        } else {
+          _.activeBreakpoint = targetBreakpoint;
           
-      n = Math.sqrt(r * r + dy * dy);
-      
-      // Draw a connective line between this two points if the distance is lesser or equal the minimum distance
-      if (n <= config.minDistance) {
-        context.beginPath();
-        context.strokeStyle = 'rgba(' + config.color.r + ', ' + config.color.g + ', ' + config.color.b + ', ' + (1.2 - n / config.minDistance) + ')';
-        context.moveTo(p1.x, p1.y);
-        context.lineTo(p2.x, p2.y);
-        context.stroke();
-        context.closePath();
+          if(_.breakpointSettings[targetBreakpoint].disable) {
+            console.log('Disable plugin');
+            //_.unslick(targetBreakpoint);
+          } else {
+            _.options = _._extend(_.options, _.breakpointSettings[targetBreakpoint]);
+            _._refresh();
+          }
+        }
+      } else {
+        if(_.activeBreakpoint !== null) {
+          _.activeBreakpoint = null;
+          targetBreakpoint = null;
+          
+          _.options = _._extend(_.options, _.originalSettings);
+          _._refresh();
+        }
       }
-    };
+    }
+  };
 
-    /**
-     * Gets called via the event handler and sets the canvas dimensions
-     */
-    this.resize = function() {
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
+
+  /**
+   * Description here…
+   * 
+   * @private
+   */
+  Plugin.prototype._refresh = function() {
+    var _ = this;
+
+    _._initializeStorage();
+    _._update();
+  };
+
+
+  /**
+   * Description here…
+   * 
+   * @private
+   */
+  Plugin.prototype._resize = function() {
+    var _ = this;
+
+    _.element.width = window.innerWidth;
+    _.element.height = window.innerHeight;
+
+    if(window.innerWidth !== _.windowWidth) {
+      clearTimeout(_.windowDelay);
+
+      _.windowDelay = window.setTimeout(function() {
+        _.windowWidth = window.innerWidth;
+        _._checkResponsive();
+      }, 50);
+    }
+  };
+
+
+  /**
+   * Description here…
+   * 
+   * @private
+   */
+  Plugin.prototype._animate = function() {
+    var _ = this;
+
+    _._draw();
+    window.requestAnimFrame(_._animate.bind(_));
+  };
+
+
+  /**
+   * Description here…
+   * 
+   * @private
+   */
+  Plugin.prototype._draw = function() {
+    var _ = this;
+
+    _.context.clearRect(0, 0, _.element.width, _.element.height);
+    
+    for (var i = _.storage.length; i--;) {
+      var particle = _.storage[i];
+      particle._draw();
+    }
+    
+    _._update();
+  };
+
+
+  /**
+   * Description here…
+   * 
+   * @private
+   */
+  Plugin.prototype._update = function() {
+    var _ = this;
+
+    for (var i = _.storage.length; i--;) {
+      var particle = _.storage[i];
+        
+      particle.x += particle.vx;
+      particle.y += particle.vy;
+        
+      if (particle.x + particle.radius > _.element.width) {
+        particle.x = particle.radius;
+      } else if (particle.x - particle.radius < 0) {
+        particle.x = _.element.width - particle.radius;
+      }
+          
+      if (particle.y + particle.radius > _.element.height) {
+        particle.y = particle.radius;
+      } else if (particle.y - particle.radius < 0) {
+        particle.y = _.element.height - particle.radius;
+      }
+        
+      if (_.options.connect) {
+        for (var j = i + 1; j < _.storage.length; j++) {
+          var particle2 = _.storage[j];
+        
+          _._calculateDistance(particle, particle2);
+        }
+      }
+    }
+  };
+
+
+  /**
+   * Description here…
+   * 
+   * @private
+   */
+  Plugin.prototype._calculateDistance = function(p1, p2) {
+    var _ = this;
+
+    var n, r = p1.x - p2.x, dy = p1.y - p2.y;  
+        n = Math.sqrt(r * r + dy * dy);
       
-      this.draw();
-    };
+      if (n <= _.options.minDistance) {
+        _.context.beginPath();
+        _.context.strokeStyle = 'rgba(' + _.options.color.r + ', ' + _.options.color.g + ', ' + _.options.color.b + ', ' + (1.2 - n / _.options.minDistance) + ')';
+        _.context.moveTo(p1.x, p1.y);
+        _.context.lineTo(p2.x, p2.y);
+        _.context.stroke();
+        _.context.closePath();
+      }
   };
 
-  Particle = function() {
-    // Setup random attributes
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.vx = Math.random() * config.speed * 2 - config.speed;
-    this.vy = Math.random() * config.speed * 2 - config.speed;
-    this.radius = Math.random() * Math.random() * config.sizeVariations;
-
-    /**
-     * The particles draw method.
-     */
-    this.draw = function() {
-      context.fillStyle = 'rgb(' + config.color.r + ', ' + config.color.g  + ', ' + config.color.b + ')';
-      context.beginPath();
-      context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      context.fill();
-    };
-
-    this.draw();
-  };
 
   /**
-   * Merge the keys of two objects and return a comined object.
-   *
-   * @param {Object} source - object with the default keys
-   * @param {Object} obj - object with additional keys who will extend the source
-   *
-   * @return {Object} combined object
+   * Description here…
+   * 
+   * @private
    */
-  function _mergeObjects(source, obj) {
+  Plugin.prototype._extend = function(source, obj) {
     Object.keys(obj).forEach(function(key) { source[key] = obj[key]; });
+    
     return source;
-  }
+  };
+
 
   /**
-   * Converts a hex string to a rgb object.
-   *
-   * @param {String} hex - hex value who should be converted
-   *
-   * @return {Object} object with all seperate color channels
+   * Description here…
+   * 
+   * @private
    */
-  function _hex2rgb(hex) {
+  Plugin.prototype._hex2rgb = function(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     
     return result ? {
@@ -200,11 +361,46 @@ var Particles = (function(window, document) {
       g: parseInt(result[2], 16),
       b: parseInt(result[3], 16)
     } : null;
-  }
+  };
+
 
   /**
-   * A polyfill for requestAnimationFrame.
+   * Description here…
+   * 
+   * @constructor
    */
+  Particle = function(element, context, options) {
+    var _ = this;
+    
+    _.element = element;
+    _.context = context;
+    _.options = options;
+
+    _.x = Math.random() * _.element.width;
+    _.y = Math.random() * _.element.height;
+    _.vx = Math.random() * _.options.speed * 2 - _.options.speed;
+    _.vy = Math.random() * _.options.speed * 2 - _.options.speed;
+    _.radius = Math.random() * Math.random() * _.options.sizeVariations;
+
+    _._draw();
+  };
+
+
+  /**
+   * Description here…
+   * 
+   * @private
+   */
+  Particle.prototype._draw = function() {
+    var _ = this;
+
+    //_.context.fillStyle = 'rgb(' + _.options.color.r + ', ' + _.options.color.g  + ', ' + _.options.color.b + ')';
+    _.context.fillStyle = _.options.color;
+    _.context.beginPath();
+    _.context.arc(_.x, this.y, _.radius, 0, Math.PI * 2, false);
+    _.context.fill();
+  };
+
   window.requestAnimFrame = (function() {
     return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame ||
       function(callback) {
@@ -212,9 +408,6 @@ var Particles = (function(window, document) {
       };
   })();
 
-  /**
-   * Return a new plugin instance.
-   */
   return new Plugin();
 })(window, document);
 
