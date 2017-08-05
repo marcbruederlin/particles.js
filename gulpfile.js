bb8da95c36c9424f6e87e774e8eaeab24f2f7259
Derek Zhou <yizi.zhou@gmail.com>
2017-08-04 20:26:32 -0700

    added fingerprinting to build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,31 @@
 (function() {
   'use strict';
-  
+
   var gulp = require('gulp'),
       gutil = require('gulp-util'),
       argv = require('yargs').argv,
       clean = require('gulp-clean'),
       jshint = require('gulp-jshint'),
       uglify = require('gulp-uglify'),
-      rename = require('gulp-rename');
-  
+      rename = require('gulp-rename'),
+      rev = require('gulp-rev');
+
   gulp.task('jshint', function() {
     return gulp.src('./src/*')
       .pipe(jshint('.jshintrc'))
       .pipe(jshint.reporter('jshint-stylish'));
   });
-  
+
   gulp.task('build:clean', function() {
     return gulp.src(['./dist'])
       .pipe(clean({force: true}));
-  });  
+  });
 
   gulp.task('build:full', ['build:clean'], function() {
     return gulp.src('./src/*')
       .pipe(gulp.dest('./dist/'));
   });
-  
+
   gulp.task('build:compressed', ['build:full'], function() {
     return gulp.src('./src/*')
       .pipe(uglify({
@@ -34,12 +35,13 @@
       }))
       .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
       .pipe(rename({suffix: '.min'}))
+      .pipe(rev())
       .pipe(gulp.dest('./dist/'));
   });
 
   if(argv.watch) {
     gulp.watch('./src/*', ['build']);
   }
-  
+
   gulp.task('build', ['jshint', 'build:clean', 'build:full', 'build:compressed']);
 }());
