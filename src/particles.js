35fba32eaf5fba559801bae73a2f26ee1d94c477
Marc Brüderlin <hello@marcbruederlin.com>
2018-08-09 20:03:25 +0200

    Bumped version number to 2.2.3

--- src/particles.js
+++ src/particles.js
@@ -2,7 +2,7 @@
  * A lightweight, dependency-free and responsive javascript plugin for particle backgrounds.
  *
  * @author Marc Bruederlin <hello@marcbruederlin.com>
- * @version 2.2.2
+ * @version 2.2.3
  * @license MIT
  * @see https://github.com/marcbruederlin/particles.js
  */
